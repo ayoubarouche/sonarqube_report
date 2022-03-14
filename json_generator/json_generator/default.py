74803e44c9e0bd11f1506e7e115bd516a49d118d
ayoubarouche <aarouche2016@gmail.com>
2022-03-14 10:21:38 +0100

    fix merge conflit

--- json_generator/json_generator/default.py
+++ json_generator/json_generator/default.py
@@ -1,1 +1,5 @@
-#
+#testing the generator : 
+import json 
+def generate_project(project):
+    print(json.dumps(project.__dict__))
+
--- tests.py
+++ tests.py
@@ -1,10 +1,6 @@
 from json_generator.models.issue import Component
 from json_generator.processing.modified_api.Component import ModifiedSonarCloudClient   
-<<<<<<< HEAD
-from json_generator.processing.issue_processing.parsing_component import add_issues_to_all_components   
-=======
 from json_generator.processing.file_cmp import get_componentKeys
->>>>>>> a62e09b82288882dd8fb7d8f8e1fb05caaaa8ca9
 
 sonarclout_url = "https://sonarcloud.io/"
 file = open("access_token.txt",'r')
@@ -22,22 +18,10 @@ issues = list(sonar.issues.search_issues(componentKeys="ayoubarouche_linux-autot
 # components = sonar.components.get_project_component_and_ancestors("ayoubarouche_linux-autotools-gh-actions-sc",branch="main")
 #cmp=Component(key=None).search_components_in_issues(componentKeys="ayoubarouche_linux-autotools-gh-actions-sc",branch="main")
 # sonar.issues.issue_set_tags("AX9vlR8A1T4myP5e-Jjo","security")
-<<<<<<< HEAD
-component = list(sonar.components.search_components(organization ="kestar",qualifiers="FIL"))
-components = list(sonar.components.get_components_tree(component="ayoubarouche_linux-autotools-gh-actions-sc", qualifiers="FIL"))
-componts = []
-comp1 = Component(key="ayoubarouche_linux-autotools-gh-actions-sc:src/hello.cpp")
-comp2 = Component(key="ayoubarouche_linux-autotools-gh-actions-sc:src/main.cpp")
-componts.append(comp1)
-componts.append(comp2)
-branch="main"
-comps = add_issues_to_all_components(sonar=sonar , components = componts , branch=branch,tags="update")
-=======
 # components = list(sonar.components.search_components(organization ="kestar",qualifiers="FIL"))
 # components = list(sonar.components.get_components_tree(component="ayoubarouche_linux-autotools-gh-actions-sc", qualifiers="FIL"))
 cmp=get_componentKeys("ayoubarouche_linux-autotools-gh-actions-sc","main","security")
 
->>>>>>> a62e09b82288882dd8fb7d8f8e1fb05caaaa8ca9
 
 # print()
 print("the list of projects is : ")
@@ -57,14 +41,9 @@ print("------------------------------------")
 # print()
 
 # print("the isssuer of is : ")
-<<<<<<< HEAD
-# print(issues)
-# # print(type(issues))
-=======
 #print(issues)
 # print(len(issues))
 # print(type(issues))
->>>>>>> a62e09b82288882dd8fb7d8f8e1fb05caaaa8ca9
 
 # print()
 # print("cmponent is :")
