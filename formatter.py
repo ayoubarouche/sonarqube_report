42e2850c0efbdad6c409336dc70171ab89fc2b94
soumia mouatassem <soumia_mouatassem@outlook.fr>
2022-03-17 14:29:45 +0100

    adding branch loop

--- formatter.py
+++ formatter.py
@@ -19,38 +19,41 @@ pdf.ln(20)
 # pdf.add_page()
 pdf.set_auto_page_break(auto=True,margin =15)
 
-pdf.second_page('output_file.json')
+
 # # print("the uuid of file is : "+str(data[0]["details"][0]["information_per_file"]))
 pdf.add_page()
 json_file = open('output_file.json','r')
 data = json.load(json_file)
 i=1
-for f in data[0]["details"][0]["information_per_file"]:
-    file = Component(key=None)
-    file.parse_jsoncomponent_from_output_file(f)
-    issues_json = f["issues"]
+for branch in data[0]["details"]:
+    pdf.second_page(branch["summary_informations"])
+    for f in branch["information_per_file"]:
+        file = Component(key=None)
+        file.parse_jsoncomponent_from_output_file(f)
+        issues_json = f["issues"]
 
-    unresolved = list(issues_json["unresolved"])
-    # print("the unresolved are : "+json.loads(str(unresolved[0]))["key"])
+        unresolved = list(issues_json["unresolved"])
+        # print("the unresolved are : "+json.loads(str(unresolved[0]))["key"])
 
-    wontfix = list(issues_json["wontfix"])
+        wontfix = list(issues_json["wontfix"])
 
-    fixed = list(issues_json["fixed"])
+        fixed = list(issues_json["fixed"])
 
-    false_positive = list(issues_json["false_positive"])
+        false_positive = list(issues_json["false_positive"])
 
-    removed = list(issues_json["removed"])
+        removed = list(issues_json["removed"])
 
-    unresolved_issues = parse_list_json_issues_to_list_json_objects(unresolved)
-    wontfix_issues = parse_list_json_issues_to_list_json_objects(wontfix)
-    fixed_issues = parse_list_json_issues_to_list_json_objects(fixed)
-    false_positive_issues = parse_list_json_issues_to_list_json_objects(false_positive)
-    removed_issues = parse_list_json_issues_to_list_json_objects(removed)
-    pdf.summaryHeader(title='General view of the file')
-    pdf.TitlesHeader(title='File Number : '+str(i))
-    pdf.add_file(file , unresolved_issues=unresolved_issues,wontfix_issues=wontfix_issues ,fixed_issues=fixed_issues , false_positive_isssues=false_positive_issues , removed_issues=removed_issues)
-    i=i+1
-#swl wach najotiw les tags tahoma ola blach 
+        unresolved_issues = parse_list_json_issues_to_list_json_objects(unresolved)
+        wontfix_issues = parse_list_json_issues_to_list_json_objects(wontfix)
+        fixed_issues = parse_list_json_issues_to_list_json_objects(fixed)
+        false_positive_issues = parse_list_json_issues_to_list_json_objects(false_positive)
+        removed_issues = parse_list_json_issues_to_list_json_objects(removed)
+        pdf.summaryHeader(title='General view of the file')
+        pdf.TitlesHeader(title='File Number : '+str(i))
+        pdf.add_file(file , unresolved_issues=unresolved_issues,wontfix_issues=wontfix_issues ,fixed_issues=fixed_issues , false_positive_isssues=false_positive_issues , removed_issues=removed_issues)
+        i=i+1
+        pdf.ln(20)
+    #swl wach najotiw les tags tahoma ola blach 
 
 pdf.output('report.pdf')
 
