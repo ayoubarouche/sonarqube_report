964d98599ae0c6486994e97d0a6b350b6fc53fe7
ayoubarouche <aarouche2016@gmail.com>
2022-03-27 21:06:34 +0000

    optimizing the get components algorithm

--- json_generator/models/issue.py
+++ json_generator/models/issue.py
@@ -139,5 +139,12 @@ class Component :
             self.key = json_str["file_key"]
         if 'file_name' in json_str :
             self.name = json_str["file_name"]
+    
+    def __hash__(self):
+        return hash((self.key, self.uuid, self.name))
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)): return NotImplemented
+        return self.key == other.key 
  
 
