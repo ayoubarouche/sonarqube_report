02a58bc8afc0fb1c4ef9a93887ccbf75726bf21e
ayoubarouche <aarouche2016@gmail.com>
2022-04-19 16:06:34 +0000

    fixing bugs

--- json_generator/processing/project_processing/project_proc.py
+++ json_generator/processing/project_processing/project_proc.py
@@ -15,6 +15,7 @@ functions :
 
 from json_generator.models.branch import Branch
 from json_generator.models.issue import Issue
+from json_generator.models.measure import Measure
 from json_generator.models.project import Project
 
 
@@ -27,7 +28,16 @@ def parse_obj_to_json(class_obj):
     json_obj = vars(class_obj)
     
     return json_obj
+    
+def get_json_of_measures(measures):
+    result = []
 
+    for measure in measures : 
+        result_measure = {}
+        result_measure["metric"] = measure.metric
+        result_measure["value"] = measure.value
+        result.append(result_measure)
+    return result
 
 #get proj 
 def get_project(sonar,arg_proj):
@@ -97,7 +107,19 @@ def get_spec_issues_of_project(sonar,arg_proj,args_branch,args_issue):
     
 
 
-
+def get_measures_of_project(sonar , args_proj , args_branch , metric_keys):
+    print("the meatrics keys are : ")
+    
+    measures_json = list(sonar.measures.get_component_with_specified_measures(component = args_proj.key , metricKeys = metric_keys , branch = args_branch.name)["component"]["measures"])
+    print("measure json are")
+    print(measures_json)
+    measures_objects = []
+    for measure_json in measures_json:
+        measure_object = Measure()
+        measure_object.parse_jsonMetric(measure_json)
+        measures_objects.append(measure_object)
+        
+    return measures_objects
 
 
 
